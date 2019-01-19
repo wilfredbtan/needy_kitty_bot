80e65d4f10ace26e156e46ea9e011037b2c94182
Wilfred Bradley Tan <wilfredbtan@gmail.com>
2019-01-20 05:33:40 +0800

    Working timer for feed and play

---
+++ index.js
@@ -0,0 +1,76 @@
+const Telegraf = require('telegraf')
+const session = require('telegraf/session')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+var appetite = 2;
+var excitement = 2;
+
+function reset() {
+  excitement = 2;
+  appetite = 2;
+}
+// function feedPlay(ctx) {
+//   return ctx.reply('<b>feed</b> or <i>play?</i>', Extra.HTML().markup((m) =>
+//     m.inlineKeyboard([
+//       m.callbackButton('feed', 'feed'),
+//       m.callbackButton('play', 'play')
+//     ])))
+// }
+function response() {
+  const rand = Math.random()
+  if (rand < 0.3) {
+    return "nyan nyan";
+  } else if (rand < 0.8) {
+    return "purr~";
+  } else {
+    return "hiss!";
+  }
+}
+function hide(ctx) {
+  editMessageReplyMarkup(ctx.from.chat_id)
+}
+
+bot.help((ctx) => ctx.reply('Meow Meow (I am a needy kitty!)'))
+bot.hears('meow', (ctx) => ctx.reply(response()))
+
+bot.start((ctx) => {
+  // console.log(ctx);
+  //feeding and playtime timer
+  setTimeout(x => {
+    return ctx.reply('<b>feed</b> or <i>play?</i>', Extra.HTML().markup((m) =>
+      m.inlineKeyboard([
+        m.callbackButton('feed', 'feed'),
+        m.callbackButton('play', 'play')
+      ])))
+  }, 10000);
+  ctx.reply('Meow meow meow meow');
+})
+
+bot.action('feed', (ctx) => {
+  appetite += 1;
+  if(appetite > 3) {
+    reset();
+    ctx.reply('Needy Kitty ate too much. Needy Kitty is dead. \nType /start to revive Needy Kitty.')
+  } else if(excitement < 0) {
+    ctx.reply('Needy Kitty starved to death. \nType /start to revive Needy Kitty.')
+  } else {
+    ctx.reply('yum yum in my tum tum');
+  }
+})
+
+bot.action('play', (ctx) => {
+  excitement += 1;
+  if(excitement > 3) {
+    reset();
+    ctx.reply('Needy Kitty died of over-excitement. \nType /start to revive Needy Kitty.')
+  } else if(excitement < 0) {
+    ctx.reply('Needy Kitty died of boredom. \nType /start to revive Needy Kitty.')
+  } else {
+    ctx.reply('meow meow (perform for me human)');
+  }
+})
+
+bot.launch()
